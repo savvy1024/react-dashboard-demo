d1754fdf19160790dd19ebf6d3a156f416147d80
Savvy <savvywang@gmail.com>
2023-04-18 19:47:05 +0800

    修改：     src/components/sidebar/Sidebar.js

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -8,22 +8,25 @@ const Sidebar = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const location = useLocation();
 
-  console.log('location:', window.location)
+  console.log("location:", window.location);
 
   useEffect(() => {
     const curPath = window.location.pathname;
     console.log(curPath);
-    const index = sidebarNav.findIndex((item) => item.link === location.pathname);
+    const index = sidebarNav.findIndex(
+      (item) => item.link === location.pathname
+    );
     setActiveIndex(index);
-  },[location])
+  }, [location]);
 
   const closeSidebar = () => {
-    document.querySelector('.main_content').style.transform = 'scale(1) translateX(0)'
+    document.querySelector(".main_content").style.transform =
+      "scale(1) translateX(0)";
     setTimeout(() => {
-        document.body.classList.remove('sidebar-open')
-        document.querySelector('.main_content').style = ''
+      document.body.classList.remove("sidebar-open");
+      document.querySelector(".main_content").style = "";
     }, 500);
-}
+  };
 
   return (
     <div className='sidebar'>
@@ -39,8 +42,7 @@ const Sidebar = () => {
             to={item.link}
             key={`nav-${index}`}
             className={`sidebar_menu_item ${activeIndex === index && "active"}
-                    }`
-            }
+                    }`}
             onClick={closeSidebar}
           >
             <div className='sidebar_menu_item_icon'>{item.icon}</div>
@@ -48,11 +50,11 @@ const Sidebar = () => {
           </Link>
         ))}
       </div>
-      <div className="sidebar_menu_item">
-        <div className="sidebar_menu_item_icon">
-          <i className="bx bx-log-out"></i>
+      <div className='sidebar_menu_item'>
+        <div className='sidebar_menu_item_icon'>
+          <i className='bx bx-log-out'></i>
         </div>
-        <div className="sidebar_menu_item_text">Logout</div>
+        <div className='sidebar_menu_item_text'>Logout</div>
       </div>
     </div>
   );
